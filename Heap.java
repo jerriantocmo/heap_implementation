19a3e339bfe80694aaab7ade7cf5fb1426a504c8
Jerrian Tocmo <Jerriant94@gmail.com>
2026-06-09 02:35:38 -0700

    Added Heap.java file, and added function skeletons

---
+++ Heap.java
@@ -0,0 +1,27 @@
+import util.util;
+
+public class Heap{
+
+    public Heap(){
+        util.print("Hello from the heap class");
+    }
+
+    private void insert(int newItem){
+
+    }
+
+    private void pop(int newItem){
+
+    }
+
+    private void findMax(int newItem){
+
+    }
+
+    private int size(){
+        int size = 0;
+
+        return size;
+    }
+
+}
