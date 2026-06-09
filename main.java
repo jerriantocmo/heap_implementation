4b5cb2b3500179ce202ffd2df854b6504501361b
Jerrian Tocmo <Jerriant94@gmail.com>
2026-06-09 03:20:20 -0700

    Testing funcionality

--- main.java
+++ main.java
@@ -1,4 +1,13 @@
+import util.util;
 
 public static void main(String args[]){
     Heap heap = new Heap();
+    heap.insert(0);
+    heap.insert(30);
+    heap.insert(23);
+    heap.insert(19);
+    heap.print();
+    heap.pop();
+    util.print("Popped");
+    heap.print();
 }
